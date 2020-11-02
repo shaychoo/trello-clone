0e638575ba5e4ef517d8ce11a9f94d891544c398
Shay Turjeman <shaytrjmn@gmail.com>
2020-11-02 12:10:51 +0200

    progress need more work

---
+++ src/column.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class column extends Component {
+  render() {
+    return this.props.column.title;
+  }
+}
---
+++ src/initial-data.js
@@ -0,0 +1,18 @@
+const initialData = {
+  tasks: {
+    "task-1": { id: "task-1", content: "Tast 1" },
+    "task-2": { id: "task-2", content: "Tast 2" },
+    "task-3": { id: "task-3", content: "Tast 3" },
+    "task-4": { id: "task-4", content: "Tast 4" },
+  },
+  columns: {
+    "column-1": {
+      id: "column-1",
+      title: "to do",
+      taskIds: ["task-1", "task-2", "task-3", "task-4"],
+    },
+  },
+  columnOrder: ["column-1"],
+};
+
+export default initialData;
